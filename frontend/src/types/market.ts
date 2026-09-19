98dbb0b447436ae3883065f3da8bc80e89131c9f
Ikumi <ikumi.w0613@gmail.com>
2026-09-19 16:23:41 +0900

    マーケットAPI統合のためにMarketDataタイプを追加n

---
+++ frontend/src/types/market.ts
@@ -0,0 +1,8 @@
+export type MarketData = {
+  gldPrice: number;
+  usdJpy: number;
+  xauUsdPrice: number;
+  xauUsdPreviousClose: number;
+  xauUsdChange: number;
+  xauUsdChangePercent: number;
+};
